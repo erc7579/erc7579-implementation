9fd505c25e004b5d8bd4303b8be450c67c479b2b
zeroknots <zeroknots@protonmail.com>
2023-12-03 11:36:29 +0700

    chore: removing unused imports

--- src/core/HookManager.sol
+++ src/core/HookManager.sol
@@ -2,13 +2,10 @@
 pragma solidity ^0.8.21;
 
 import "./ModuleManager.sol";
-import "sentinellist/SentinelList.sol";
 import "../interfaces/IMSA.sol";
 import "../interfaces/IModule.sol";
 
 abstract contract HookManager is ModuleManager, IMSA_ConfigExt {
-    using SentinelListLib for SentinelListLib.SentinelList;
-
     IHook public _hook;
 
     /**
