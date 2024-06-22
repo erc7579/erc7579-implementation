7374cb6d0efabae12bb008b08100f272fdc682d3
kopy-kat <kopp.konrad@gmail.com>
2024-06-22 20:55:10 +0200

    chore: add natspec

--- src/core/RegistryAdapter.sol
+++ src/core/RegistryAdapter.sol
@@ -4,6 +4,12 @@ pragma solidity ^0.8.23;
 import { IERC7484 } from "../interfaces/IERC7484.sol";
 import { AccountBase } from "./AccountBase.sol";
 
+/**
+ * @title RegistryAdapter
+ * @author kopy-kat | rhinestone.wtf
+ * @dev This contract uses ERC-7484 to check if a module is attested to and exposes a modifier to
+ * use it.
+ */
 abstract contract RegistryAdapter is AccountBase {
     event ERC7484RegistryConfigured(address indexed smartAccount, address indexed registry);
 
